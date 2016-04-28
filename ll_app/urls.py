baa3e9e69b9c865c71243e412eae43b171c2626e
Justin Hearn <justin@hearn.me>
2016-04-28 13:27:46 -0700

    got topics pages working

--- ll_app/urls.py
+++ ll_app/urls.py
@@ -6,4 +6,10 @@ from . import views
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+    
+    # Show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+    
+    # Details page for a single topic
+    url(r'^topics/(?P<topic_id>[0-9]+)/$', views.topic, name='topic'),
 ]
--- ll_app/views.py
+++ ll_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """The home page for unroots LL"""
     return render(request, 'll_app/index.html')
+
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request, 'll_app/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic,'entries':entries}
+    return render(request, 'll_app/topic.html', context)
