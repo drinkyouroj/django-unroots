961b940086dff104c0a631ef0c827923133afe17
Justin Hearn <justin@hearn.me>
2016-04-28 14:14:58 -0700

    add topics, add/edit entries

--- ll_app/views.py
+++ ll_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Topic
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
 
 def index(request):
     """The home page for unroots LL"""
@@ -19,3 +23,61 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic,'entries':entries}
     return render(request, 'll_app/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('ll_app:topics'))
+            
+    context = {'form':form}
+    return render(request, 'll_app/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = EntryForm()
+    else:
+        # POST data submitted; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('ll_app:topic', 
+                args=[topic_id]))
+            
+    context = {'topic':topic,'form':form}
+    return render(request, 'll_app/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('ll_app:topic', 
+                args=[topic.id]))
+                
+    context = {'entry':entry,'topic':topic,'form':form}
+    return render(request, 'll_app/edit_entry.html', context)
+    
